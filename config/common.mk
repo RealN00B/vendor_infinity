RisingOS-staging/android_vendor_lineage

etc: Add bootanim init
Signed-off-by: Alvin Francis <nivlafx@gmail.com>

config/common.mk
@@ -141,6 +141,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
+# Bootanim
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/init/init.bootanim.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.bootanim.rc
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig \

RealN00B/vendor_infinity

config/common.mk
@@ -124,6 +124,10 @@ PRODUCT_PACKAGES += \
     product_charger_res_images \
     product_charger_res_images_vendor
 
+# Bootanim
+PRODUCT_COPY_FILES += \
+    vendor/infinity/prebuilt/common/etc/init/init.bootanim.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.bootanim.rc
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig \