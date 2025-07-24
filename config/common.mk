Krishna-s-Hub/android_vendor_pixelage

config: Install FRP wipe script into recovery /system/bin Change-Id: I24e217e6af87f2002193ac7b6defb158cce0a776

config/common.mk
@@ -170,6 +170,10 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/%/libfuse-lite.so \
     system/%/libntfs-3g.so
 
+# FRP
+PRODUCT_COPY_FILES += \
+    vendor/pixelage/prebuilt/common/bin/wipe-frp.sh:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/wipe-frp
+
 # Openssh
 PRODUCT_PACKAGES += \
     scp \

RealN00B/vendor_infinity

config/common.mk
@@ -168,6 +168,10 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 # Freeform Windows
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
+# FRP
+ PRODUCT_COPY_FILES += \
+     vendor/infinity/prebuilt/common/bin/wipe-frp.sh:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/wipe-frp
     
 # Gamespace
 PRODUCT_PACKAGES += \